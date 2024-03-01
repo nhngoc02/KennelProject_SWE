e70419c6321d06a5b714f9eee128d7189e9457b7
Audrey Newman <audreyrnew@gmail.com>
2024-03-01 13:04:36 -0600

    Login and Signup now with correct templates

--- kennel_link/app.js
+++ kennel_link/app.js
@@ -2,23 +2,37 @@ const express = require('express')
 const morgan = require("morgan");
 const methodOverride = require("method-override");
 //const mongoose = require("./database");
-const app = express()
+
+let livereload = require("livereload");
+let connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 require("dotenv").config({path:'../data.env'});
 
-app.set('view engine', 'ejs')
+const app = express()
 
+app.set('view engine', 'ejs')
+app.use(connectLiveReload());
 app.use(morgan("tiny"));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static("static"));
 app.use('/styles', express.static('./styles'));
 
+
+
 app.get('/', (req, res) => {
     res.render('pages/homepage')
 })
 
 app.get("/signup", (req, res) => {
-    res.render("./pages/signup");
+    res.render("pages/signup");
 })
 
 app.get("/login", (req,res) => {
@@ -26,7 +40,7 @@ app.get("/login", (req,res) => {
 })
 
 app.get("/clientReservation", (req, res) => {
-    res.render("ClientReservation.ejs");
+    res.render("ClientReservation");
 })
 
 
