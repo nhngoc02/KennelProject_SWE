34d76cae4606e347c169b0d79f40d06a688e0018
Audrey Newman <audreyrnew@gmail.com>
2024-03-06 14:32:41 -0600

    Added login route
    Added login route to app.js and started making the authenticate function: have not gotten it working yet but wanted to push so it is updated.

--- kennel_link/app.js
+++ kennel_link/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const methodOverride = require("method-override");
 const mongoose = require("./database");
+const Client = require("./db_modules/client")
+const Employee = require("./db_modules/employee")
 
 let livereload = require("livereload");
 let connectLiveReload = require("connect-livereload");
@@ -23,9 +25,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static("static"));
 app.use('/styles', express.static('./styles'));
 
-// function authenticate(name, pass) {
+async function authenticate(name, pass, user_type) {
+  if(user_type = 'client') {
+    const username = await Client.find({client_username: name}) 
+    if() {
 
-// }
+    }
+  } else if(user_type = 'employee') {
+    const username = await Employee.find({emp_username: name})
+
+  }
+}
 
 app.get('/', (req, res) => {
   console.log("Displaying homepage")
@@ -40,8 +50,16 @@ app.get("/login", (req,res) => {
   res.render("pages/login");
 })
 
-app.post("/login", (req,res) => {
+app.post("/login", async (req,res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    const user_type = req.body.user_type;
+    authenticate(username, password, user_type);
 
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 })
 
 app.get("/home", (req,res) => {
