877ec7dc373c4ab0e911737a404124b9cf14b10c
Tiare Delgado <tdelgado@trinity.edu>
2024-04-19 13:24:06 -0500

    test file updated

--- kennel_link/tests/app.test.js
+++ kennel_link/tests/app.test.js
@@ -1,12 +1,15 @@
 const app = require('../app.js');
 const request = require('supertest');
+const session = require('supertest-session');
 
 
 describe('App', () => {
   let server;
   let mongoServer;
+  let testSession;
 
   beforeAll(done => {
+    testSession = session(app);
     server = app.listen(4000, () => {
             console.log('Test server is running on port 4000');
       done();
@@ -15,6 +18,7 @@ describe('App', () => {
 
   afterAll(done => {
     server.close(done);
+    testSession.destroy();
   });
 
   it('should start without crashing', async () => {
@@ -25,26 +29,56 @@ describe('App', () => {
     expect(response.status).toBe(200);
   });
 
-  it('sign up a new employee successfully', async () => {
-    const employeeData = {
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'john.doe@example.com',
-      phone: '1234567890',
-      employee_start_date: '2024-03-30',
-      username: 'johndoe',
-      password: 'password123'
+  it('should login employee successfully', async () => {
+    const employeeCredentials = {
+      username: 'tdelgado',
+      password: 'SHAKEIT2023',
+      user_type: 'employee'
     };
-
-    const response = await request(server)
-      .post('/employeesignup')
-      .send(employeeData);
-
-    // Check if the response status is 201 (Created)
-    expect(response.status).toBe(201);
-    // Check if the response text contains the success message
-    expect(response.text).toContain('Employee signup successful!');
+  
+    // Send a POST request to the login endpoint using the session instance
+    const response = await testSession
+      .post('/login')
+      .send(employeeCredentials);
+  
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('pages/dashboard');
   });
 
+  it('should login client successfully', async () => {
+    const clientCredentials = {
+      username: 'janesmith',
+      password: 'password2',
+      user_type: 'client'
+    };
+
+    const response = await request(app)
+      .post('/login')
+      .send(clientCredentials);
+
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('pages/dashboard');
+  });
+
+
+  //test sign up for employee and client 
+  // - should also test for creation of duplicate accounts 
+
+  // test client and employee pet search functionality 
+
+  // test that clients cannot search other clients
+
+  // test employee client search functionality 
+
+  // tests reservation creation 
+  // - should not allow for duplicate reservations
+
+  // test employee reservations search (should show all existing reservations)
+
+  // test client reservation search (should show only their reservations)
+
+  // test employee and client remove pet functionality 
+
+  // test employee remove client functionality 
 });
 
